24652998bf31350d7058ce0a498ff4faf3b64804
Israel Zablianov <123hartk123@gmail.com>
2017-07-18 00:24:30 +0300

    removing photos manager to new branch

--- Photo Master/src/app/types/feature.ts
+++
@@ -1,6 +0,0 @@
-export interface Feature {
-    name?: string;
-    description?: string;
-    iconUrl?: string;
-    routeUrl?: string;
-}
--- Photo Master/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import './index.css';
-import App from "./app/components/app/app";
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
